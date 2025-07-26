c9c36fb36a438aacd20d7580194d65d0fdb86f98
ludovic ngono <lg.ngono@gmail.com>
2025-07-26 14:15:17 +0100

    update 12

--- src/app/theme/utils/translate-universal-loader.ts
+++ src/app/theme/utils/translate-universal-loader.ts
@@ -1,16 +1,17 @@
 import {TranslateLoader} from '@ngx-translate/core';
-import { Observable } from "rxjs";
+import {Observable} from "rxjs";
+
 const fs = require('fs');
 
 export class TranslateUniversalLoader implements TranslateLoader {
-     constructor(
-    public authService: AuthService,
-    public snackBar:MatSnackBar,private prefix: string = 'i18n', private suffix: string = '.json') {}
+  constructor(
+    private prefix: string = 'i18n', private suffix: string = '.json') {
+  }
 
-    public getTranslation(lang: string): Observable<any> {
-        return new Observable((observer:any) => {
-            observer.next(JSON.parse(fs.readFileSync(`${this.prefix}/${lang}${this.suffix}`, 'utf8')));
-            observer.complete();
-        });
-    }
+  public getTranslation(lang: string): Observable<any> {
+    return new Observable((observer: any) => {
+      observer.next(JSON.parse(fs.readFileSync(`${this.prefix}/${lang}${this.suffix}`, 'utf8')));
+      observer.complete();
+    });
+  }
 }
